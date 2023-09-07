c3eceb203a70b02c0595c2592f02035ce36d7dac
JanBrzozowski <jan.b.brzozowski@gmail.com>
2023-09-07 23:16:24 +0200

    array

---
+++ src/data_structs/array.c
@@ -0,0 +1,60 @@
+#include "array.h"
+#include "value.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// make array of values
+val_array_p make_val_array() {
+	val_array_p a = malloc(sizeof(*a));
+	if (a == NULL) {
+        	fprintf(stderr, "Failed to allocate memory for val_array");
+        	exit(EXIT_FAILURE);
+    	}
+	a->size = 2;
+	a->num_of_elements = 0;
+	a->elements = malloc(sizeof(value_p) * a->size);
+	if (a->elements == NULL) {
+        	fprintf(stderr, "Failed to allocate memory for val_array elements");
+        	free(a);  
+		exit(EXIT_FAILURE);
+    	}
+	return a;
+}
+
+// add value to the array of values
+void add_2_val_array(val_array_p a, value_p val) {
+	if (a->size == a->num_of_elements) {
+		increase_val_array(a);
+	}
+	a->elements[a->num_of_elements] = val;
+	a->num_of_elements++;
+}
+
+// increase the size of the array of values
+void increase_val_array(val_array_p a) {
+	a->size *= 2;
+	value_p* new_array = malloc(sizeof(value_p) * a->size);
+	if (new_array == NULL) {
+        	fprintf(stderr, "Failed to allocate memory for increased val_array");
+        	exit(EXIT_FAILURE);
+    	}
+	for(int i = 0; i < a->num_of_elements; i++) {
+		new_array[i] = a->elements[i];
+	}
+
+	free(a->elements);
+	a->elements = new_array;	
+}
+
+// free memory that had been allocated for the array of values
+void free_val_array(val_array_p a) {
+	if(a != NULL) {
+		if(a->elements != NULL) {
+			for(int i = 0; i < a->num_of_elements; i++) {
+				free_value(a->elements[i]);
+			}
+			free(a->elements);
+		}
+		free(a);
+	}
+}
---
+++ src/data_structs/array.h
@@ -0,0 +1,22 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include "value.h"
+#include <stdio.h>
+
+typedef struct val_array {
+	value_p* elements;      // elements in array
+	size_t size;            // size allocated for the arrey
+	size_t num_of_elements; // actual number of elements in the array
+} val_array_t, *val_array_p;
+
+// make array of values
+val_array_p make_val_array();
+// add value to the array of values
+void add_2_val_array(val_array_p a, value_p val);
+// increase the size of the array of values
+void increase_val_array(val_array_p a);
+// free memory that had been allocated for the array of values
+void free_val_array(val_array_p a);
+
+#endif
