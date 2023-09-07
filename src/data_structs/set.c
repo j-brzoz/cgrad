ba3a90f44ca96e50d3ff68282f85015b7dabe7df
JanBrzozowski <jan.b.brzozowski@gmail.com>
2023-09-07 23:16:43 +0200

    set

---
+++ src/data_structs/set.c
@@ -0,0 +1,70 @@
+#include "set.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// make a set
+set_p make_set_p() {
+	set_p s = malloc(sizeof(*s));
+	if (s == NULL) {
+        	fprintf(stderr, "Failed to allocate memory for set.");
+        	exit(EXIT_FAILURE);
+    	}
+	s->size = 2;
+	s->num_of_elements = 0;
+	s->elements = malloc(sizeof(size_t) * s->size);
+	if (s->elements == NULL) {
+        	fprintf(stderr, "Failed to allocate memory for set elements.");
+        	free(s);  
+		exit(EXIT_FAILURE);
+    	}
+	return s;
+}
+
+// add an element to the set
+size_t add_2_set(set_p s, size_t val) {
+	if(check_if_in_set(s, val) == 1) {
+		return 0;
+	} else {
+		if (s->size == s->num_of_elements) {
+			increase_set(s);
+		}
+		s->elements[s->num_of_elements] = val;
+		s->num_of_elements++;
+		return 1;
+	}
+}
+
+// check if element is already in the set
+size_t check_if_in_set(set_p s, size_t val) {
+	for(int i = 0; i < s->num_of_elements; i++) {
+		if(s->elements[i] == val) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+// double the size of set
+void increase_set(set_p s) {
+	s->size *= 2;
+	size_t* new_set = malloc(sizeof(size_t) * s->size);
+	if (new_set == NULL) {
+        	fprintf(stderr, "Failed to allocate memory for increased set.");
+        	exit(EXIT_FAILURE);
+    	}
+	for(int i = 0; i < s->num_of_elements; i++) {
+		new_set[i] = s->elements[i];
+	}
+	free(s->elements);
+	s->elements = new_set;	
+}
+
+// free memory that had been allocated for the set
+void free_set(set_p s) {
+	if(s != NULL) {
+		if(s->elements != NULL) { 
+			free(s->elements);
+		}
+		free(s);
+	}
+}
---
+++ src/data_structs/set.h
@@ -0,0 +1,23 @@
+#ifndef SET_H
+#define SET_H
+
+#include <stdio.h>
+
+typedef struct set {
+	size_t* elements;       // elements
+	size_t size;            // memory allocated for elements
+	size_t num_of_elements; // actual number of elements
+} set_t, *set_p;
+
+// make a set
+set_p make_set_p();
+// add an element to the set
+size_t add_2_set(set_p s, size_t val);
+// check if element is already in the set 
+size_t check_if_in_set(set_p s, size_t val);
+// double the size of set
+void increase_set(set_p s);
+// free memory that had been allocated for the set
+void free_set(set_p s);
+
+#endif
